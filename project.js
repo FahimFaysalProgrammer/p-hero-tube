0fcf1f2d95311dbdf59a12d826fe4fa852e654e4
FahimFaysalProgrammer <fahim.faysal.programmer@gmail.com>
2023-11-30 17:41:13 +0600

    Menubar section completed

---
+++ project.js
@@ -0,0 +1,60 @@
+const loadData = (global) => {
+    // console.log("Hello world!");
+    const searchText = document.getElementById("search-text").value;
+    //   console.log(searchText);
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?f=${
+        searchText ? searchText : global
+      }`
+    )
+      .then((res) => res.json())
+      // .then(data => console.log(data.meals))
+      .then((data) => displayData(data.meals));
+  };
+  
+  const displayData = (data) => {
+    // console.log(data);
+    document.getElementById("total-meals").innerText = data.length;
+  
+    const mealsContainer = document.getElementById("meals-container");
+  
+    data.forEach((meal) => {
+      // console.log(meal);
+      const card = document.createElement("div");
+      card.classList.add("meals-card");
+      card.innerHTML = `
+          <img class="box-img" src=${meal?.strMealThumb} alt="">
+          <h4>${meal?.strMeal}</h4>
+          <p>${meal.strInstructions.slice(0, 72)}</p>
+          <button
+              onclick="displayModal('${meal.idMeal}')"
+              type="button"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              >
+              Details
+          </button>
+          `;
+      mealsContainer.appendChild(card);
+    });
+  };
+  
+  const displayModal = async (id) => {
+    //   console.log("hello world");
+    //   console.log(id);
+    try {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      const data = await response.json();
+      console.log(data.meals[0]);
+    } catch {
+      (err) => {
+        console.log(err);
+      };
+    }
+  };
+  
+  loadData("a");
+  
